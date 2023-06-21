20fa9642257b6c1f845bc8f3a1de6e8657c86b60
Edwin Soto <125782999+Calculated-Risk@users.noreply.github.com>
2023-06-21 18:06:12 -0400

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Add your code here
+
+//function submitData which takes two strings, userName & userEmail
+function submitData(userName, userEmail){
+
+    //write a POST req to http://localhost:3000/users using fetch()
+    return fetch('http://localhost:3000/users', {
+            method: "POST",
+            headers:{
+                "Content-Type": "application/json",
+                "Accept": "application/json"
+            },
+            body: JSON.stringify({
+                name: `${userName}`,
+                email: `${userEmail}`
+            }),  
+    })
+    .then(function(object){
+        return object.json();
+    })
+    .then(function(data){
+        const bod = document.querySelector('body')                               
+        bod.textContent = data.id
+    })
+    .catch(function(error){
+        const bodyy = document.querySelector('body')                               
+        bodyy.textContent = error
+    })
+
+};
